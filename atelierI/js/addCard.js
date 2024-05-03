8e46c2f889dacaef5a1731c6278807dc3af5dc4b
Okzie <okzieyka.dev@gmail.com>
2024-05-03 11:55:22 +0200

    Added values of cursor

--- atelierI/js/addCard.js
+++ atelierI/js/addCard.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    var postUrl = 'http://tp.cpe.fr:8083/card'; // Stocke l'URL dans une variable
+
+    // Met à jour les valeurs initiales des curseurs
+    $('#hpValue').text($('#hp').val());
+    $('#energyValue').text($('#energy').val());
+    $('#attackValue').text($('#attack').val());
+    $('#defenceValue').text($('#defence').val());
+
     $(".ui.form").on('submit', function(event){
         event.preventDefault();
         
@@ -18,7 +26,7 @@ $(document).ready(function(){
             family: data['card-category'],
             affinity: data['card-affinity'],
             imgUrl: data['card-url'],
-            smallImgUrl: data['card-url2'], // Utilise la même URL pour l'instant
+            smallImgUrl: data['card-url2'],
             energy: parseInt(data['energy']),
             hp: parseInt(data['hp']),
             defence: parseInt(data['defence']),
@@ -30,10 +38,10 @@ $(document).ready(function(){
 
         // Envoi des données en POST
         $.ajax({
-            url: 'http://tp.cpe.fr:8083/card',
+            url: postUrl,
             type: 'POST',
             data: JSON.stringify(apiData),
-            contentType: 'application/json', // Indique que nous envoyons du JSON
+            contentType: 'application/json',
             success: function(data){
                 console.log(data);
                 window.location.href = "http://localhost/cards.html";
@@ -43,5 +51,19 @@ $(document).ready(function(){
                 console.log('Détails de l\'erreur:', err.responseText);
             },
         });
+    });
+
+    // Met à jour les valeurs des curseurs lorsque l'utilisateur les déplace
+    $('#hp').on('input', function() {
+        $('#hpValue').text($(this).val());
+    });
+    $('#energy').on('input', function() {
+        $('#energyValue').text($(this).val());
+    });
+    $('#attack').on('input', function() {
+        $('#attackValue').text($(this).val());
+    });
+    $('#defence').on('input', function() {
+        $('#defenceValue').text($(this).val());
     });
 });
